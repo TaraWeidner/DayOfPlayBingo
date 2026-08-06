c27018c7716681623497fd062895b38614923179
TaraWeidner <taratalnenetz@gmail.com>
2026-08-05 20:36:11 -0700

    Add event configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Big Day of Play event configuration.
+ *
+ * The game works immediately without a database. Progress is saved locally on
+ * each player's device. To enable the shared TV celebration display, add a
+ * Firebase Realtime Database URL below and publish again.
+ */
+window.DAY_OF_PLAY_CONFIG = {
+  eventId: "big-day-of-play-2026",
+  eventName: "Big Day of Play",
+  organizationName: "Inclusive Health",
+  playUrl: "https://taraweidner.github.io/DayOfPlayBingo/",
+  firebaseDatabaseUrl: ""
+};
